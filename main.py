938ba03c8ad314021ea86481df9b1bb1bdef7031
Julien Bourseau <julien.bourseau@grenoble-inp.org>
2022-02-05 12:59:56 +0100

    Début du main, algo naif commencé

--- main.py
+++ main.py
@@ -3,6 +3,34 @@
 from timeit import timeit
 from sys import argv
 from geo.point import Point
+from itertools import combinations
+import math
+import geo.tycat
+import geo.segment as segment
+
+#### Différents algorithmes possibles:
+
+def algo_naif(points):
+    couples = combinations(points, 2)
+    dist_min = math.inf  #on initialise proprement
+    for couple in couples:
+        dist = Point.distance_to(couple[0], couple[1])
+        if dist < dist_min:
+            dist_min = dist
+    return dist
+
+def display_naif(points):
+    couples = combinations(points, 2)
+    dist_min = math.inf  #on initialise proprement
+    geo.tycat.tycat(points)
+    for couple in couples:
+        dist = Point.distance_to(couple[0], couple[1])
+        if dist < dist_min:
+            dist_min = dist
+            nouv_seg = segment.Segment([couple[0], couple[1]])
+            geo.tycat.tycat(*[points, nouv_seg])
+    return dist
+
 
 
 def load_instance(filename):
@@ -11,7 +39,6 @@ def load_instance(filename):
     returns list of points.
     """
     with open(filename, "r") as instance_file:
-        line = next(iter(instance_file))
         points = [Point((float(p[0]), float(p[1]))) for p in (l.split(',') for l in instance_file)]
 
     return points
@@ -21,7 +48,8 @@ def print_solution(points):
     """
     calcul et affichage de la solution (a faire)
     """
-    pass  # TODO: afficher la solution
+    display_naif(points)
+
 
 
 def main():
@@ -31,7 +59,6 @@ def main():
     for instance in argv[1:]:
         points = load_instance(instance)
         print_solution(points)
-
 
 main()
 
