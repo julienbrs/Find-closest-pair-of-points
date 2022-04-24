fa03af035f71a9ea56834e47e7c4f2402262b443
Julien Bourseau <julien.bourseau@grenoble-inp.org>
2022-04-24 16:17:20 +0000

    Delete generation_pts.py

--- generation_pts.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from sys import argv
-import random
-for _ in range(int(argv[1])):
-     x = random.uniform(-10000,10000)
-     y = random.uniform(-10000,10000)
-     print(x, ", ", y)
